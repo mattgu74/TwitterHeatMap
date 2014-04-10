8a60b2306772d43f9f42d2f985b93a8f27611773
Matthieu Guffroy <mattgu74@gmail.com>
2014-04-10 17:08:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Twitter HeatMap
+===============
+
+Publish a heatmap of tweet filtered by location or text.
+
+Installation
+************
+
+> virtualenv env
+> source env/bin/activate
+> pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,45 @@
+# -*- coding: utf8 -*-
+from twython import TwythonStreamer
+from config import CONF
+from pprint import pprint
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit, BaseNamespace
+import gevent
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+
+class MyStreamer(TwythonStreamer):
+    def __init__(self, *args, **kwargs):
+        TwythonStreamer.__init__(self, *args, **kwargs)
+
+    def on_success(self, data):
+        emit('tweet', data)
+
+    def on_error(self, status_code, data):
+        print status_code, data, "STOP MySTREAMER !!"
+        self.disconnect()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    stream = MyStreamer(CONF['APP_KEY'], CONF['APP_SECRET'],
+                    CONF['OAUTH_TOKEN'], CONF['OAUTH_TOKEN_SECRET'])
+    stream.statuses.filter(locations="0,45,5,50")
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    try:
+        socketio.run(app)
+    except KeyboardInterrupt:
+        pass
