8ca2152f9df87ca2fa8b5777260b93e16db9820b
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2014-04-10 22:34:11 +0200

    Shared twitter streamer

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 # -*- coding: utf8 -*-
-from twython import TwythonStreamer
-from config import CONF
+from gevent import monkey; monkey.patch_all()
+
 from pprint import pprint
-from flask import Flask, render_template
+
+import gevent
+from twython import TwythonStreamer
+import flask
+from flask import Flask, render_template, request, g, current_app
 from flask.ext.socketio import SocketIO, emit, BaseNamespace
+from werkzeug.local import LocalProxy
+
+from config import CONF
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-
 class MyStreamer(TwythonStreamer):
     def __init__(self, *args, **kwargs):
         TwythonStreamer.__init__(self, *args, **kwargs)
+        print('INIT '*10)
+        self.queues = {}
 
     def on_success(self, data):
-        emit('tweet', data)
+        for queues in self.queues.values():
+            queues.put_nowait(data)
 
     def on_error(self, status_code, data):
         print status_code, data, "STOP MySTREAMER !!"
         self.disconnect()
+
+    def get_queue(self, uid):
+        return self.queues.setdefault(uid, gevent.queue.Queue())
+
+    def delete_queue(self, uid):
+        del self.queues[uid]
+
+
+streamer = MyStreamer(CONF['APP_KEY'], CONF['APP_SECRET'],
+                CONF['OAUTH_TOKEN'], CONF['OAUTH_TOKEN_SECRET'])
+green = gevent.spawn(streamer.statuses.filter, locations="-5,42.2,8.13,51")
 
 
 @app.route('/')
@@ -29,13 +50,20 @@ def index():
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    stream = MyStreamer(CONF['APP_KEY'], CONF['APP_SECRET'],
-                    CONF['OAUTH_TOKEN'], CONF['OAUTH_TOKEN_SECRET'])
-    stream.statuses.filter(locations="-5,42.2,8.13,51")
+    uid = request.namespace.socket.sessid
+    print('Client %s connected' % uid)
+    queue = streamer.get_queue(uid)
+    while True:
+        tweet = queue.get(timeout=60)
+        emit('tweet', tweet)
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    print('Client disconnected')
+    uid = request.namespace.socket.sessid
+    print('Client %s disconnected' % uid)
+    streamer.delete_queue(uid)
+
 
 if __name__ == '__main__':
     try:
